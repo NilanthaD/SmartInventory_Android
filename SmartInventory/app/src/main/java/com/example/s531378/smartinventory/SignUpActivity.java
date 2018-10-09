0ac6c9f574de9b1943fe0019d033dafbbd83da10
Pittu <S531378@nwmissouri.edu>
2018-10-09 00:48:13 -0500

    Created SignUpActivity

---
+++ SmartInventory/app/src/main/java/com/example/s531378/smartinventory/SignUpActivity.java
@@ -0,0 +1,13 @@
+package com.example.s531378.smartinventory;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignUpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
