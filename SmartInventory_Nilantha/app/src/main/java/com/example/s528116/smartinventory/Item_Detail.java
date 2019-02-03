85f82b7f295b731d3a0a61843198aec445575da4
Nilantha, Dambadeni Kalu Achchillage <s528116@nwmissouri.edu>
2019-02-03 14:01:59 -0600

    Completed the UI for item detail view and user supply request.

--- SmartInventory_Nilantha/app/src/main/java/com/example/s528116/smartinventory/Item_Detail.java
+++ SmartInventory_Nilantha/app/src/main/java/com/example/s528116/smartinventory/Item_Detail.java
@@ -1,7 +1,12 @@
 package com.example.s528116.smartinventory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Item_Detail extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class Item_Detail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item__detail);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.logout:
+
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(Item_Detail.this, MainActivity.class);
+                startActivity(i);
+//                return (true);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
