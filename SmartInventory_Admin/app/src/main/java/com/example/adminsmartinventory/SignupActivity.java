d9909e1ea8dbe3c777a71f06a09f329cd4c506ea
shivani busireddy <shivanireddy30@gmail.com>
2019-02-03 18:39:22 -0600

    Added logo.png

--- SmartInventory_Admin/app/src/main/java/com/example/adminsmartinventory/SignupActivity.java
+++ SmartInventory_Admin/app/src/main/java/com/example/adminsmartinventory/SignupActivity.java
@@ -1,7 +1,9 @@
 package com.example.adminsmartinventory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        public void SignupFn(View v)
+        {
+            Intent in = new Intent(this,MainActivity.class);
+            startActivity(in);
+        }
     }
 }
