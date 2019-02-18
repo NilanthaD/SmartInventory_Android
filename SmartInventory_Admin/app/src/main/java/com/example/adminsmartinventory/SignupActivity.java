fadc1835bacb697a8a72782f2f32995bb98458d6
Niharika-19 <s531368@nwmissouri.edu>
2019-02-18 00:51:43 -0600

    Added signup for admin

--- SmartInventory_Admin/app/src/main/java/com/example/adminsmartinventory/SignupActivity.java
+++ SmartInventory_Admin/app/src/main/java/com/example/adminsmartinventory/SignupActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
     private EditText adminIdET;
@@ -45,11 +47,42 @@ public class SignupActivity extends AppCompatActivity {
                 String passWord = passwordET.getText().toString();
                 String fName = fNameET.getText().toString();
                 String lName = lNameET.getText().toString();
+
+
+                if(!passWord.equals(confirmPWET.getText().toString())){
+                    setAlert("Password conformation failed. ");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(userName)){
+                    Toast.makeText(Signup.this, "Email cannot be empty", Toast.LENGTH_SHORT).show();
+                    setAlert("Email cannot be empty");
+                    return;
+                }
+                if(TextUtils.isEmpty(passWord)){
+//                    Toast.makeText(Signup.this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
+                    setAlert("Password cannot be empty");
+                    return;
+                }
+                if(passWord.length()<6){
+//                    Toast.makeText(signup, "Password should be at least 6 charactors", Toast.LENGTH_SHORT).show();
+                    setAlert("Password should be at least 6 characters");
+                    return;
+                }
+                if(TextUtils.isEmpty(fName) || TextUtils.isEmpty(lName) || TextUtils.isEmpty(pNumber)){
+                    setAlert("All the fields need to be filed.");
+                    return;
+                }
+
+
+
             }
 
 
 
     }
+
+
 
 
 
