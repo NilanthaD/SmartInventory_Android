34560e914b6ba6955d9c851b5814eb1209803360
Nilantha, Dambadeni Kalu Achchillage <s528116@nwmissouri.edu>
2019-04-08 12:41:20 -0500

    updates

--- SmartInventory_Nilantha/app/src/main/java/com/example/s528116/smartinventory/ItemsAdapter.java
+++ SmartInventory_Nilantha/app/src/main/java/com/example/s528116/smartinventory/ItemsAdapter.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
