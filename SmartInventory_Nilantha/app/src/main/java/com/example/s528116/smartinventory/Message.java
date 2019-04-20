a4590fbcd63bc85816dc77d4d05bd5e403065226
karthikv21 <s531510@nwmissouri.edu>
2019-04-19 23:39:11 -0500

    Have edited the Message.java file

--- SmartInventory_Nilantha/app/src/main/java/com/example/s528116/smartinventory/Message.java
+++ SmartInventory_Nilantha/app/src/main/java/com/example/s528116/smartinventory/Message.java
@@ -2,12 +2,29 @@ package com.example.s528116.smartinventory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Message extends AppCompatActivity {
+
+    private TextView titleTV, senderTV, sendDateTV;
+
+   // private Button replyBTN;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        titleTV = findViewById(R.id.itemNameTV);
+        sendDateTV = findViewById(R.id.sendDateTV);
+        senderTV = findViewById(R.id.senderTV);
+        
+        db = FirebaseFirestore.getInstance();
+
     }
 }
