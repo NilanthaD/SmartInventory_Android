8b875b5d976d3d37056c35180a94b5c1e77c4f95
Lokeswari Pittu <s531378@nwmissouri.edu>
2019-04-22 03:42:52 -0500

    commit

--- SmartInventory_Admin/app/src/main/java/com/example/adminsmartinventory/MainActivity.java
+++ SmartInventory_Admin/app/src/main/java/com/example/adminsmartinventory/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.adminsmartinventory;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,8 +26,9 @@ public class MainActivity extends AppCompatActivity {
     EditText passwordET;
     String username, password;
 
-
+    private CheckBox rememberMeCB;
     private FirebaseAuth mAuth;
+    private SharedPreferences rememberMe;
 
 
     @Override
@@ -37,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         userNameET = findViewById(R.id.userNameET);
         passwordET = findViewById(R.id.passwordET);
         loginBTN = findViewById(R.id.loginBTN);
+        rememberMeCB = findViewById(R.id.rememberMeCB);
+        rememberMe = MainActivity.this.getPreferences(MODE_PRIVATE);
 
 //        FirebaseApp.initializeApp(this);
       mAuth = FirebaseAuth.getInstance();
@@ -61,10 +66,19 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+                            if(rememberMeCB.isChecked()){
+
+                                SharedPreferences.Editor  editor = rememberMe.edit();
+                                editor.putString("userName", username);
+                                editor.putString("password", password);
+                                editor.commit();
+                            }
 
                             //Toast.makeText(MainActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
                             Intent init = new Intent(MainActivity.this, AdminView.class);
                             startActivity(init);
+
+
                         } else {
                             Toast.makeText(MainActivity.this, "login failed", Toast.LENGTH_SHORT).show();
 
--- SmartInventory_Admin/app/src/main/java/com/example/adminsmartinventory/UploadShippingLabel.java
+++ SmartInventory_Admin/app/src/main/java/com/example/adminsmartinventory/UploadShippingLabel.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
